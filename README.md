316e89a981b0a737320a26745fa7b6167e50b68d
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2013-06-19 15:38:19 -0400

    Skeleton node server set up for running static HTML sites quickly

---
+++ README.md
@@ -0,0 +1,21 @@
+# RWD Lab
+
+A place to test out responsive web design techniques and tools. 
+
+## Get the server up and running
+
+You'll need **node** and **npm**, which you can install on OS X using Homebrew or any other way you want. Once you have them, getting the server running is:
+
+```
+$ cd {project dir}
+$ npm install
+$ node bin/server.js {port, default 8000}
+```
+
+Load up <http://localhost:8000> and you'll see your server running.
+
+## Building new sections and files
+
+Create a root home page at public/index.html and any other folders and pages you want inside public, accessing them like "public/your/path/index.html" -> <http://localhost:8000/your/path>
+
+
---
+++ bin/server.js
@@ -0,0 +1,40 @@
+var fs = require("fs");
+var host = "127.0.0.1";
+var port = process.argv[2] || 8000;
+var express = require("express");
+
+var app = express();
+app.use(app.router); //use both root and other routes below
+app.use(express.static(__dirname + "/public")); //use static files in ROOT/public folder
+
+app.get("/", function (request, response) {
+    var index = './public/index.html';
+    try { fs.readFileSync(index); }
+    catch (e) {
+        response.send("<h1>The RWD Lab Server is running</h1><p>Create a file at" + index + "</p>");
+        return;
+    }
+    response.sendfile(index);
+});
+
+app.get("/*", function(request, response){
+    var file = './public' + request.url;
+
+    try { fs.readFileSync(file); }
+    catch (e) {
+        file += "/index.html";
+        try { fs.readFileSync(file); }
+        catch (e) {
+            response.statusCode = 404;
+            response.send("(404) This file doesn't exist: " + file);
+            return;
+        }
+    }
+
+    response.sendfile(file);
+});
+
+console.log("Server running on localhost, port " + port);
+console.log("Press ^C to shutdown");
+
+app.listen(port, host);
